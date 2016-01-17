0f18495b041e3cf6c797055cfa7a2fb0dcf1df7f
Chris Archibald <carchi8py@gmail.com>
2016-01-16 21:29:59 -0800

    Show images in our table view.

--- Albums/AlbumsTableViewController.swift
+++ Albums/AlbumsTableViewController.swift
@@ -40,23 +40,40 @@ class AlbumsTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return albumNames.count > 0 ? 1 : 0
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return albumNames.count
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! AblumsTableViewCell
+        
+        assetsCollections = albumsCollections[indexPath.row] as! PHAssetCollection
+        photoAsset = PHAsset.fetchAssetsInAssetCollection(assetsCollections, options: nil)
+        
+        if photoAsset.count > 0 {
+            let totalImages = photoAsset.count
+            let randomIndex: Int = Int(arc4random_uniform(UInt32(photoAsset.count)))
+            let asset: PHAsset = photoAsset[randomIndex] as! PHAsset
+            
+            PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: PHImageManagerMaximumSize, contentMode: PHImageContentMode.AspectFit, options: nil, resultHandler: { (image: UIImage?, object: [NSObject: AnyObject]?) -> Void in
+                
+                cell.albumImage.image = image
+                cell.albumCount.text = totalImages > 1 ? "\(totalImages) Images" : "\(totalImages) Image"
+            })
+        } else {
+            cell.albumImage.image = UIImage(named: "imagePlaceholder")
+            cell.albumCount.text = "0 Images"
+        }
+        cell.albumName.text = albumNames[indexPath.row]
 
-        // Configure the cell...
+        
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
