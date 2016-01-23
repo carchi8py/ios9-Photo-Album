61b97e07e322dc8b502b20efd951d5f01701c7ee
Chris Archibald <carchi8py@gmail.com>
2016-01-23 13:19:03 -0800

    Code is Borken moving from table view to collection view.

---
+++ Albums/AlbumCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCollectionViewCell.swift
+//  Albums
+//
+//  Created by Chris Archibald on 1/23/16.
+//  Copyright © 2016 Chris Archibald. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
--- Albums/ViewController.swift
+++ Albums/ViewController.swift
@@ -9,20 +9,46 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     var albumName: String!
     var photoAssets: PHFetchResult!
     var assetsCollection: PHAssetCollection!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.navigationItem.title = albumName
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        collectionView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Mark: Collection View Data Source Methods
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoAssets.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: AlbumCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! AlbumCollectionViewCell
+        
+        let asset: PHAsset = photoAssets[indexPath.row] as! PHAsset
+        PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: PHImageManagerMaximumSize, contentMode: PHImageContentMode.AspectFit, options: nil) { (image: UIImage?, object: [NSObject: AnyObject]?) -> Void in
+            cell.imageView.image = image
+            
+        }
+        return cell
     }
 
 
